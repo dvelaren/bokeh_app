17adf73eca466d3822814dabd82eccba99ba308b
David Velasquez <dvelas25@eafit.edu.co>
2019-09-18 07:48:32 +0000

    Cleaned code

--- bokeh_app.py
+++ bokeh_app.py
@@ -1,14 +1,18 @@
+import logging
 from flask import Flask, render_template
 from bokeh.embed import server_document
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route("/")
+@app.route("/")
 def index():
-    # tag = server_document(url=r'/bokeh', relative_urls=True)
-    tag = server_document(url='http://sise.dis.eafit.edu.co/bokeh')
-    # tag = server_document(url='http://192.168.10.130:5006/bokeh')
-    return render_template('index.html', tag=tag, context=Flask)
+    tag = server_document(url=r'/bokeh', relative_urls=True)
+    return render_template('index.html', tag=tag)
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
+
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dvelas25/bokeh_app/venv"
+set -gx VIRTUAL_ENV "/home/dvelasquez/bokeh_app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
